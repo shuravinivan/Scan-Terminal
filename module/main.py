0369df6be1c7aad7dcb34c9edf408f909dbf9e6e
ivansuravin <shuravin&ivan@gmail.com>
2025-03-18 23:13:37 +0400

    Рабочая версия, Внесены изменения в файл main, добавлена  приветственная строка

--- module/main.py
+++ module/main.py
@@ -3,6 +3,8 @@ import work_mail
 
 
 def main():
+    print('Good afternoon!')
+    print('Scan Terminal online.')
     schedule.every(10).minutes.do(work_mail.mail)
     while True:
         schedule.run_pending()
