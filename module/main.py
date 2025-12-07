5e6d86ada3c563d77101fe405c4c897ff0389be5
ivansuravin <shuravin&ivan@gmail.com>
2025-12-08 01:31:53 +0400

    Изменение способа задания периодичности опроса.

--- module/main.py
+++ module/main.py
@@ -1,12 +1,17 @@
 import schedule
 import work_mail
+import time
 
 
 def main():
-    print('Good afternoon!' + '\n' + 'Scan Terminal online.' + '\n')
-    schedule.every(10).minutes.do(work_mail.mail)
+    print('Добрый день!' + '\n' + 'Терминал активен.' + '\n')
+    # schedule.every(10).minutes.do(work_mail.mail)
+    # work_mail.mail()
+    # time.sleep(600)
     while True:
-        schedule.run_pending()
+        work_mail.mail()
+        time.sleep(600)
+        # schedule.run_pending()
 
 if __name__ == "__main__":
     main()
