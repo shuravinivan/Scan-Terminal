4d3d0928a4704ee772e6accd648f0a28a28c657a
ivansuravin <shuravin&ivan@gmail.com>
2025-12-08 02:06:41 +0400

    Косметические изменения.

--- module/main.py
+++ module/main.py
@@ -5,7 +5,7 @@ import time
 
 def main():
     data = time.strftime('%d/%m/%Y %H:%M')
-    print('\n' + 'Добрый день! Сканер терминала активен.' + data + '\n')
+    print('\n' + 'Добрый день! Сканер терминала активен. ' + data + '\n')
     # schedule.every(10).minutes.do(work_mail.mail)
     while True:
         work_mail.mail()
