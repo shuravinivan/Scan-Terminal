945c27baf603bca2cb698a5a404d735874fab7d4
ivansuravin <shuravin&ivan@gmail.com>
2026-03-23 17:32:56 +0400

    Косметичекская правка кода

--- module/send_max_mess.py
+++ module/send_max_mess.py
@@ -2,29 +2,25 @@ import requests
 from config import format_date
 import time
 
-
-
-
 # text = 'Удача с нами'
 # chat_id = '-72298581395976'
 
-
-def send_mess(text, chat_id):
-
-    time.sleep(2)
-    data = time.strftime(format_date)
-    message = str(' '.join(text))
-    print(data)
-    print(message)
-
-    from config import API
-    from config import TOKEN_MAX
-    from config import user_id
-
-    r = requests.post(f"{API}/messages?chat_id={chat_id}",
-                      headers={"Authorization": TOKEN_MAX, "Content-Type": "application/json"},
-                      json={"text": f"{message}"})
-
-    print('\n')
+try:
+    def send_mess(text, chat_id):
+        time.sleep(2)
+        data = time.strftime(format_date)
+        message = str(' '.join(text))
+        print(data)
+        print(message)
+        from config import API
+        from config import TOKEN_MAX
+        from config import user_id
+        r = requests.post(f"{API}/messages?chat_id={chat_id}",
+                          headers={"Authorization": TOKEN_MAX, "Content-Type": "application/json"},
+                          json={"text": f"{message}"})
+        print('\n')
+except:
+    print(time.strftime(format_date))
+    print('Ошибка модуля отправки сообщений', '\n')
 
 # send_mess(chat_id,text)
